16147d7530c9b08a76e82d3f9f4fed165d09764e
Vinh <nguyenvinh1997@yahoo.de>
2020-05-18 04:00:29 +0700

    Cart mockup

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -2,7 +2,30 @@ import React from 'react'
 
 const Cart = () => {
     return(
-        <h1>Cart</h1>
+        <div className ="container">
+            <div className = "cart">
+                <h5>You have ordered:</h5>
+                <div className = "collection">
+                    <p>Nothing.</p>
+                </div>
+            </div>
+            <div className = "container">
+                <div className = "collection">
+                    <li className = "collection-item">
+                        <label>
+                            <input type = "checkbox" />
+                            <span>Shipping (+6€)</span>
+                        </label>
+                    </li>
+                    <li className = "collection-item">
+                        <b>Total: xxx €</b>
+                    </li>
+                </div>
+                <div className = "checkout">
+                    <button>Checkout</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return(
         <div className = "navbar">
-            <ul>
-                <li className = "title">Avocado Store</li>
-                <li><Link to = {'/cart'}>My Cart</Link></li>
-                <li><Link to = {'/'}>Shop</Link></li>
-            </ul>
+            <div className = "container">
+                <p className = "title">Avocado Store</p>
+                <ul>
+                    <li><Link to = {'/cart'}>My Cart</Link></li>
+                    <li><Link to = {'/'}>Shop</Link></li>
+                </ul>
+            </div>
         </div>
     )
 }
