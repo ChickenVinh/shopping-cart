b3743bf238c9c29538381a8c44043deaeb5c197b
Vinh <nguyenvinh1997@yahoo.de>
2020-05-20 02:33:02 +0700

    show added items in cart overview

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,34 +1,41 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import {Link} from 'react-router-dom'
 
 const Cart = () => {
     const state = useSelector(state => state)
+    const addedItems =state.addedItems.length ?
+        (state.addedItems.map(item =>{
+        return(
+            <li className = 'collection-item avatar' key = {item.id}>
+                <div className = "item-img">
+                    <img src = {item.img} alt = {item.img} className = ''/>
+                </div>
+                <div className = 'item-desc'>
+                    <span className = 'title'>{item.title}</span>
+                    <p>{item.desc}</p>
+                    <p><b>Price: {item.price}€</b></p>
+                    <p><b>Quantity: {item.quantity}</b></p>
+                    <div className = 'add-remove'>
+                        <Link to='/cart'><i className='material-icons'>arrow_drop_up</i></Link>
+                        <Link to='/cart'><i className='material-icons'>arrow_drop_down</i></Link>
+                    </div>
+                    <button className = 'waves-effect waves-light btn pink remove'>Remove</button>
+                </div>
+            </li>
+        )
+    })):
+    (<p>Nothing.</p>)
+
     return(
-        <div className ="container">
-            <div className = "cart">
+       <div className = 'container'>
+           <div className = 'cart'>
                 <h5>You have ordered:</h5>
-                <div className = "collection">
-                
-                    <p>Nothing.</p>
-                </div>
-            </div>
-            <div className = "container">
-                <div className = "collection">
-                    <li className = "collection-item">
-                        <label>
-                            <input type = "checkbox" />
-                            <span>Shipping (+6€)</span>
-                        </label>
-                    </li>
-                    <li className = "collection-item">
-                        <b>Total: {state.total} €</b>
-                    </li>
-                </div>
-                <div className = "checkout">
-                    <button>Checkout</button>
-                </div>
-            </div>
-        </div>
+                <ul className = 'collection'>
+                    {addedItems}
+                </ul>
+           </div>
+       </div>
     )
 }
 
